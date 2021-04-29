176efa4f82ad95cdcb854c7d0714853771453e9f
bxyexcellence <1985088145@qq.com>
2021-04-29 15:02:11 +0800

    '发现页面'

---
+++ src/pages/discover/index.js
@@ -0,0 +1,28 @@
+import React, { memo } from 'react'
+import { DiscoverWrapper, TopMenu } from './style'
+import { discoverMenu} from '@/common/local-data'
+import { NavLink } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+
+export default memo(function Discover(props) {
+  const {route} = props
+  //console.log(route);
+  return (
+    <DiscoverWrapper>
+      <div className="top">
+        <TopMenu className="wrap-v1">
+          {
+            discoverMenu.map(item => {
+              return (
+                <div className="item" key={item.title}>
+                  <NavLink to={item.link}>{item.title}</NavLink>
+                </div>
+              )
+            })
+          }
+        </TopMenu>
+      </div>
+      {renderRoutes(route.routes)}
+    </DiscoverWrapper>
+  )
+})
